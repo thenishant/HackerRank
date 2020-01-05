4b3dbd4662c3cd99d562357ebef22c78166ab625
Nishant <Nishat.s.aux@go-jek.com>
2020-01-05 23:19:14 +0530

    Added Viral_Advertising and Utopian_Tree

--- src/algorithms/implementation/Viral_Advertising.java
+++ src/algorithms/implementation/Viral_Advertising.java
@@ -1,19 +1,36 @@
 package algorithms.implementation;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Viral_Advertising {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int n = s.nextInt();
-        int result = 0;
-        for(int i=2;i<n+2;i++){
-            result+=i;
+    static int viralAdvertising(int n) {
+        int totalLikes = 2;
+        int todayLikes = 2;
+        if (n == 1)
+            return todayLikes;
+        for (int i = 1; i < n; i++) {
+            todayLikes = (todayLikes * 3) / 2;
+            totalLikes += todayLikes;
         }
-        int sum=0;
-        for(int i =2;i<2+n;i++){
-            sum = sum+i;
-        }
-        System.out.println(sum);
+        return totalLikes;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int result = viralAdvertising(n);
+        System.out.println(result);
+//        bufferedWriter.write(String.valueOf(result));
+//        bufferedWriter.newLine();
+//
+//        bufferedWriter.close();
+
+        scanner.close();
     }
 }
